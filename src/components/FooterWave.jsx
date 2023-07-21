96c252d77143848b0a4b5f69b4eb7cd9534a16e7
Ankan112 <ankanhalder112@gmail.com>
2023-07-21 21:51:52 +0600

    footer done

---
+++ src/components/FooterWave.jsx
@@ -0,0 +1,25 @@
+const FooterWave = () => {
+  return (
+    <div>
+      <svg
+        width="100%"
+        height="100%"
+        id="svg"
+        viewBox="0 0 1440 250"
+        xmlns="http://www.w3.org/2000/svg"
+        className="transition duration-300 ease-in-out delay-150"
+      >
+        <path
+          d="M 0,400 C 0,400 0,200 0,200 C 110.46428571428572,184.60714285714286 220.92857142857144,169.21428571428572 354,181 C 487.07142857142856,192.78571428571428 642.7500000000001,231.75000000000003 756,235 C 869.2499999999999,238.24999999999997 940.0714285714287,205.78571428571428 1047,194 C 1153.9285714285713,182.21428571428572 1296.9642857142858,191.10714285714286 1440,200 C 1440,200 1440,400 1440,400 Z"
+          stroke="none"
+          strokeWidth="0"
+          fill="#FDF6E7"
+          fillOpacity="1"
+          className="transition-all duration-300 ease-in-out delay-150 path-0"
+        ></path>
+      </svg>
+    </div>
+  );
+};
+
+export default FooterWave;
--- src/sheared/Footer.jsx
+++ src/sheared/Footer.jsx
@@ -1,8 +1,105 @@
+import { Link } from "react-router-dom";
+import { BsLinkedin } from "react-icons/bs";
+import { FaFacebookSquare } from "react-icons/fa";
+import { BsGithub } from "react-icons/bs";
+import { BsTwitter } from "react-icons/bs";
+import ShopifyImage from "../assets/Shopify_svg.webp";
+import FooterWave from "../components/FooterWave";
+
 const Footer = () => {
+  const year = new Date().getFullYear();
   return (
-    <div>
-      <h1 className="text-center text-lg my-3">Footer will be added soon...</h1>
-    </div>
+    <>
+      <FooterWave></FooterWave>
+      <div className="bg-[#FDF6E7]">
+        <div className="max-w-screen-xl py-8 w-10/12 mx-auto">
+          <div className="text-center">
+            <h2 className="text-xl font-bold color-primary">Bullzpot.com</h2>
+          </div>
+          <div className="h-[1px] w-full bg-[#E7E0D9] my-14"></div>
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-5">
+            <div className="flex justify-center items-center">
+              <div className="text-center">
+                <div className="grid grid-cols-4 md:grid-cols-3 lg:grid-cols-4 gap-2">
+                  <Link
+                    to="/about-us"
+                    className="text-lg font-medium hover:underline"
+                  >
+                    <span>About</span>
+                  </Link>
+                  <Link
+                    to="/reviews "
+                    className="text-lg font-medium hover:underline"
+                  >
+                    <span>Reviews</span>
+                  </Link>
+                  <Link
+                    to="/pricing"
+                    className="text-lg font-medium hover:underline"
+                  >
+                    <span>Pricing</span>
+                  </Link>
+                  <Link
+                    to="/support"
+                    className="text-lg font-medium hover:underline"
+                  >
+                    <span>Support</span>
+                  </Link>
+                </div>
+                <div className="flex justify-center gap-8 mt-5">
+                  <a href="/https://www.linkedin.com/" target="_blank">
+                    <BsLinkedin
+                      size={20}
+                      className=" color-primary"
+                    ></BsLinkedin>
+                  </a>
+                  <a href="/https://www.facebook.com/" target="_blank">
+                    <FaFacebookSquare
+                      size={20}
+                      className=" color-primary"
+                    ></FaFacebookSquare>
+                  </a>
+                  <a href="/https://www.github.com/" target="_blank">
+                    <BsGithub size={20} className=" color-primary"></BsGithub>
+                  </a>
+                  <a href="/https://www.twitter.com/" target="_blank">
+                    <BsTwitter size={20} className=" color-primary"></BsTwitter>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="flex justify-center items-center text-center">
+              <div className="">
+                <p className="text-xl font-medium ">Clean Size Charts</p>
+                <p className="text-base mt-2 font-normal hidden md:block">
+                  &copy; {year} Bullzpot, All rights reserved.
+                </p>
+              </div>
+            </div>
+            <div className="flex justify-center text-center">
+              <div>
+                <div className="flex justify-center">
+                  <img src={ShopifyImage} alt="" />
+                </div>
+                <p className="my-2">
+                  Grow your business, <br /> Create an online shop.
+                </p>
+                <button className="bg-[#95BF47] mt-2 py-2 px-6 rounded-md font-semibold text-lg color-white">
+                  <a href="/https://www.shopify.com/" target="_blank">
+                    Try it free today
+                  </a>
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="text-center block md:hidden mt-6">
+            <p className="text-base font-normal">
+              &copy; {year} Bullzpot, All rights reserved.
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
