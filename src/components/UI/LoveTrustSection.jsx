fddb62dd3c0ae55f3186fa1163f33d645c870dc6
Ankan112 <ankanhalder112@gmail.com>
2023-07-22 12:28:40 +0600

    company overview

--- src/components/UI/LoveTrustSection.jsx
+++ src/components/UI/LoveTrustSection.jsx
@@ -2,10 +2,10 @@ const LoveTrustSection = () => {
   return (
     <div className="max-w-screen-xl w-10/12 mx-auto">
       <div className="text-center">
-        <h1 className="text-[40px] color-primary font-bold">Loved & Trust</h1>
+        <h1 className="text-[40px] color-primary font-bold">Loved & Trusted</h1>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-5 mt-10">
-        <div className="h-auto border-solid border-[1px] border-gray-200 p-5 rounded-xl shadow-lg">
+      <div className="columns-1 md:columns-2 gap-5 mt-10">
+        <div className="break-inside-avoid mb-5 border-solid border-[1px] border-gray-200 p-5 rounded-xl shadow-lg">
           <p className="text-base">
             Very quick and responsive, implementation was done very nicely.
             <span className="bg-orange-200 bg-opacity-30">
@@ -15,7 +15,31 @@ const LoveTrustSection = () => {
           <h2 className="font-xl font-bold color-secondary mt-3">Vincent D.</h2>
           <span className="text-sm">Klarheit</span>
         </div>
-        <div className="border-solid border-[1px] border-gray-200 p-5 rounded-xl shadow-lg">
+
+        <div className="break-inside-avoid mb-5 border-solid border-[1px] border-gray-200 p-5 rounded-xl shadow-lg">
+          <p className="text-base">
+            <span className="bg-orange-200 bg-opacity-50">
+              Very well, quickly and effectively.
+            </span>
+            To the point . They try to understand the problem and if something
+            is not clear, they ask for additional information and makes sure
+            they understand everything.
+          </p>
+          <h2 className="font-xl font-bold color-secondary mt-3">Vincent D.</h2>
+          <span className="text-sm">Klarheit</span>
+        </div>
+
+        <div className="break-inside-avoid mb-5 border-solid border-[1px] border-gray-200 p-5 rounded-xl shadow-lg">
+          <p className="text-base">
+            <span className="bg-orange-200 bg-opacity-50">
+              Very experienced in handling the tasks involved
+            </span>
+            and capable of providing practical solutions.
+          </p>
+          <h2 className="font-xl font-bold color-secondary mt-3">Michael C.</h2>
+          <span className="text-sm">Klarheit</span>
+        </div>
+        <div className="break-inside-avoid mb-5 border-solid border-[1px] border-gray-200 p-5 rounded-xl shadow-lg">
           <p className="text-base">
             It was very important for us to have this task completed by a
             specific date. I contacted Ariana to bring her attention to it and
@@ -31,50 +55,26 @@ const LoveTrustSection = () => {
           </h2>
           <span className="text-sm">Klarheit</span>
         </div>
-        <div className="border-solid border-[1px] border-gray-200 p-5 rounded-xl shadow-lg">
+        <div className="break-inside-avoid mb-5 border-solid border-[1px] border-gray-200 p-5 rounded-xl shadow-lg">
           <p className="text-base">
+            Everything went very smoothly. Thank you for your attention on our
+            small task.
             <span className="bg-orange-200 bg-opacity-50">
-              Very well, quickly and effectively.
+              We will be back with future
             </span>
-            To the point . They try to understand the problem and if something
-            is not clear, they ask for additional information and makes sure
-            they understand everything.
+            enhancements to our site. Thanks again to the team!t
           </p>
-          <h2 className="font-xl font-bold color-secondary mt-3">Vincent D.</h2>
+          <h2 className="font-xl font-bold color-secondary mt-3">Michael C.</h2>
           <span className="text-sm">Klarheit</span>
         </div>
-        <div className="border-solid border-[1px] border-gray-200 p-5 rounded-xl shadow-lg">
+        <div className="break-inside-avoid mb-5 border-solid border-[1px] border-gray-200 p-5 rounded-xl shadow-lg">
           <p className="text-base">
             <span className="bg-orange-200 bg-opacity-50">
-              Very experienced in handling the tasks involved
+              Gagan is seriously the best!
             </span>
-            and capable of providing practical solutions. Lorem, ipsum dolor sit
-            amet consectetur adipisicing elit. Repellat ipsum eos non corrupti
-            aspernatur. Consectetur quod, quis aut vero consequuntur tempore
-            eaque corrupti, ratione nihil quaerat repellendus ipsam numquam
-            distinctio. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Fugit asperiores amet accusamus. Praesentium iusto culpa perferendis
-            quo reprehenderit, inventore blanditiis, harum quas, provident quis
-            suscipit vitae tempora laborum alias omnis aperiam aliquam?
-            Cupiditate, commodi doloremque dicta fugit doloribus ullam deleniti
-            minima rerum odit unde sapiente quo ex! Porro consectetur
-            voluptatibus, soluta facere hic quas architecto nesciunt doloremque
-            non laudantium eum iure odit. Maiores iste error ipsum sunt
-            consequatur modi placeat nobis non sed tempora? Ea aperiam magnam,
-            mollitia facilis, quaerat repellat nobis dolorum laudantium aliquid
-            recusandae architecto at et nemo vero accusamus tenetur minus
-            repudiandae. Delectus eum praesentium quos iure accusantium cumque
-            neque sunt explicabo labore dolores molestiae facere, repellat
-            officiis cum animi laboriosam recusandae? Cupiditate voluptatibus
-            quibusdam labore neque, nisi inventore vero porro modi maxime.
-            Labore rem excepturi explicabo, exercitationem odio, atque voluptate
-            eveniet incidunt maxime quae similique quas beatae eius magnam ipsum
-            fuga dolorum blanditiis. Vero, eius. Facilis, obcaecati. Earum sed
-            deleniti officia laudantium? Necessitatibus impedit optio expedita
-            saepe rerum, laboriosam nobis qui dolor harum quos iste alias ipsum
-            minus consequatur earum hic itaque illo veritatis quam vel
-            laudantium velit molestias veniam minima? Deserunt ea ex odit velit
-            beatae cum magnam. Dolores totam ipsum qui esse aliquam quod?
+            He always handles my tasks in a quick and timely manner. He also
+            goes above and beyond for me when I have revisions. I seriously can
+            not thank him enough!
           </p>
           <h2 className="font-xl font-bold color-secondary mt-3">Michael C.</h2>
           <span className="text-sm">Klarheit</span>
